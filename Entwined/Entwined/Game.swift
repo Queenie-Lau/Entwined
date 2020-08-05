6e26f17146d1e21643f76e223c83f44dbe1e1573
Queenie Lau <49220327+Queenie-Lau@users.noreply.github.com>
2020-08-05 11:54:38 -0700

    Fixed positioning of subviews
    - Figure out why button won't segue

---
+++ Entwined/Entwined/Game.swift
@@ -0,0 +1,105 @@
+//
+//  Game.swift
+//  Entwined
+//
+//  Created by Queenie Lau on 8/4/20.
+//  Copyright © 2020 Queenie Lau. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class Game: UIViewController {
+ 
+    @IBAction func clickMe(_ sender: UIButton) {
+    }
+    //    @IBAction func delay(_ sender: Any) {
+//        self.performSegue(withIdentifier: "FirstCard", sender: self)
+//    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//       if segue.identifier == "FirstCard" {
+//           if let destination = segue.destination as? Game {
+//
+//                destination.nomb = Button[(sender as! UIButton).tag] // Using button Tag
+//           }
+//       }
+//    }
+    var countdownComplete = false
+    var playedOnce = false
+    @IBOutlet weak var animationView: AnimationView!
+    var orientations = UIInterfaceOrientationMask.landscape
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+    get { return self.orientations }
+    set { self.orientations = newValue }
+    }
+    
+    func lottieAnimation(){
+        
+         let countdown = Animation.named("countdown")
+         let lottieView = AnimationView(animation: countdown)
+
+         self.view.addSubview(lottieView)
+         lottieView.contentMode = .scaleAspectFit
+         lottieView.play(toFrame: .infinity)
+         countdownComplete = true
+         playedOnce = true
+         lottieView.translatesAutoresizingMaskIntoConstraints = false
+         NSLayoutConstraint.activate([
+             lottieView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+             lottieView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+             lottieView.topAnchor.constraint(equalTo: self.view.topAnchor),
+             lottieView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+         ])
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if(playedOnce == false){
+            lottieAnimation()
+        }
+        // Get main screen bounds
+        let screenSize: CGRect = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        let yourView = UIView(frame: CGRect(x: 0, y: 0, width: screenHeight, height: screenWidth))
+        yourView.layer.borderWidth = 10
+        yourView.layer.cornerRadius = 40
+        yourView.layer.borderColor = UIColor(red:233/255, green:247/255, blue:171/255, alpha: 1).cgColor
+        view.addSubview(yourView)
+     
+    }
+//    func transitionAfterCountdown() {
+//        delay(UIButton())
+//         print("transitioned\\\\e")
+//    }
+//        if(countdownComplete == true){
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//                self.performSegue(withIdentifier: "FirstCard", sender: self)
+//            }
+//        }
+//    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
+//      self.performSegue(withIdentifier: "FirstCard", sender: self)
+//    }
+        
+//     func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//
+//            if (segue.identifier == "FirstCard") {
+//               let viewController = segue.destination as? FirstCard
+//                viewController!.OrderData = self.MyorderArray[selectedIndex]
+//                print("ran")
+//
+//            }
+//                print("didn't run")
+//        }
+    
+            
+//    @objc func authenticate(){}
+    
+//    @IBAction func Button(_ sender: Any) {
+//        if(countdownComplete == true){
+//            performSegue(withIdentifier: "Countdown", sender: nil)
+//        }
+//    }
+}
+
